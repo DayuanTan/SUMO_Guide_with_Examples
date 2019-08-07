a707c66ae3bbc6c868b8ab0d5b7423ec1ed6c53b
DayuanTan <zhuceafter2015@163.com>
2019-08-06 17:41:04 -0700

    add tutorial

---
+++ docs/tutorial/sumolympics/runner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Eclipse SUMO, Simulation of Urban MObility; see https://eclipse.org/sumo
+# Copyright (C) 2008-2019 German Aerospace Center (DLR) and others.
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+
+# @file    runner.py
+# @author  Daniel Krajzewicz
+# @author  Michael Behrisch
+# @date    2007-10-25
+# @version $Id$
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+import os
+import subprocess
+import sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+from sumolib import checkBinary  # noqa
+sumoBinary = checkBinary('sumo')
+# run simulation
+retcode = subprocess.call(
+    [sumoBinary, "-c", "data/sumolympics.sumocfg", "--no-step-log"], stdout=sys.stdout, stderr=sys.stderr)
+print(">> Simulation closed with status %s" % retcode)
+sys.stdout.flush()
